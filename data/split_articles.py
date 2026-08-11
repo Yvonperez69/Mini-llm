6e60d6e4cb7384bb6f0d3845e44d34a358d9d464
Yvon Perez <yvon.perez69@gmail.com>
2026-08-11 21:13:19 +0200

    J1 lundi split article

--- data/split_articles.py
+++ data/split_articles.py
@@ -1,5 +1,14 @@
+from pathlib import Path
 
-def split(article):
-    ...
+train_path = Path(__file__).resolve().parent / "train.txt" 
+val_path = Path(__file__).resolve().parent  / "val.txt" 
+
+
+def split_articles(data_path) : 
+    with open(data_path, "r") as f : 
+        articles = f.readlines()
+    return articles 
+
+print(len(split_articles(train_path))) 
 
 
